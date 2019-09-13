c1320eac23a5696538234d4134f5399d94d2d64f
Hiroo.Ando <Hiroo.Ando@sony.com>
2019-09-13 16:28:32 +0900

    add logback setting

--- src/main/java/dev/itatyo/studyspringboot/hello/HelloController.java
+++ src/main/java/dev/itatyo/studyspringboot/hello/HelloController.java
@@ -1,12 +1,16 @@
 package dev.itatyo.studyspringboot.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @RequestMapping("/")
     public String index() {
+        logger.info("start HelloController");
         return "Greetings from Spring Boot!";
     }
 }
